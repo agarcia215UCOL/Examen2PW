9323e99e23a5142d6fee5343fda7a3dd807481a2
sofia <sofia@SOFIAG>
2025-10-21 14:50:59 -0600

    Agregar archivos de proyecto.

---
+++ Examen2PW/Examen2PW/Components/Models/ClaseProducto.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace Examen2PW.Components.Models;
+
+public class Producto
+{
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    public string? Nombre { get; set; }
+    [Required(ErrorMessage = "El inventario debe ser un valor entero mayor a 0")]
+    public string? Inventario { get; set; }
+    [Required(ErrorMessage = "Debe seleccionar un departamento")]
+    public string? Departamento { get; set; }
+}
+
